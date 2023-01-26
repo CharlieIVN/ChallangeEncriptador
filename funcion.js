c5f9d87f292983e3b260a864ada8b9e2ef46c30d
Charlie IVN <brinkcore1@gmail.com>
2023-01-26 14:02:15 -0600

    mejorando el codigo

--- funcion.js
+++ funcion.js
@@ -7,31 +7,35 @@ function encriptar (encriptacion){
  let codigos = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
   encriptacion = encriptacion.toLowerCase ();
 
-for(let i = 0; i < codigos.length; i++ ){
-//if para buscar si en la frase existen las letras a remplazar y las devuelva con el valor del indice 1
-//remplaza pocision [0] por [1] y retorna el nuevo valor
+  for(let i = 0; i < codigos.length; i++ ){
+   //if para buscar si en la frase existen las letras a remplazar y las devuelva con el valor del indice 1
+   //remplaza pocision [0] por [1] y retorna el nuevo valor
     if (encriptacion.includes(codigos[i][0])){
      encriptacion = encriptacion.replaceAll(codigos[i][0],codigos[i][1])
     }
 }
-return encriptacion;   
+       return encriptacion;   
 }
 //funcion encriptar
+ textomsj = document.querySelector("h3");
+ visibilityCopy = document.querySelector(".copybutton");
 function btnencrip(){
-    const textomsj = document.querySelector("h3")
-    const textoencriptar = encriptar(textseccion1.value)
-    textseccio2.value = textoencriptar
-
+    const textoencriptar = encriptar(textseccion1.value);
+    textseccio2.value = textoencriptar;
     if (textseccion1.value === ""){
-        alert("No a ingresado ningun texto")
+        alert("No a ingresado ningun texto");
     }
     else{
-    textseccion1.value = ""
-    textseccio2.style.backgroundImage ="none"
-    textomsj.style.color = "white"
+    textseccion1.value ="";
+    textseccio2.style.backgroundImage ="none";
+    textomsj.style.display = "none";
+    visibilityCopy.style.display = "block"
+    
+    
+
+
 }  
 }
-
 //funcion desencriptar 
 function desencriptar (desencriptacion){
     let codigosDesencriptar = [["enter","e"],["imes","i"],["ai","a"],["ober","o"],["ufat","u"]];
@@ -52,15 +56,23 @@ function desencriptar (desencriptacion){
         alert("No a ingresado ningun texto")
     }
     else{
-    textseccion1.value = ""
-    textseccio2.style.backgroundImage ="none"
-    textomsj.style.color = "white"
+        textseccion1.value = "";
+        textseccio2.style.backgroundImage ="none";
+        textomsj.style.display = "none";
+        visibilityCopy.style.display = "block"
+    
 }  
 }
 
+
 //funcion copiar
 const copiar = document.querySelector("#text1")
- function btncopiar (){
-navigator.clipboard.writeText(copiar.value)
+  function btncopiar (){
+    navigator.clipboard.writeText(copiar.value)
+    copiar.value = "";
+    textseccio2.style.backgroundImage ="";
+    textomsj.style.display = "";
+    visibilityCopy.style.display = "none"
+
 }
 
